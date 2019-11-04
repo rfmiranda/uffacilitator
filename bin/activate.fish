986fac1299c108d0636a21f75de60595ae9589a0
Rafael Miranda <rfmiranda@stefanini.com>
2019-11-04 13:17:56 -0300

    Material Table - Disciplina

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rafael/Projetos/uffacilitator"
+set -gx VIRTUAL_ENV "/home/rafael/projetos/suelen/uffacilitator"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
